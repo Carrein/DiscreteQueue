c7a0343b6efe6a346586f0b62709f9c4c5284894
Addison Ho <addisonho@outlook.com>
2017-10-11 14:45:46 +0800

    Added javadocs for Queue.

--- Queue.java
+++ Queue.java
@@ -1,45 +1,101 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Queues are generate for each server to store customers FIFO style.
  */
 
 class Queue{
-  private final List<Customer> list;
-  private final int id;
-  private static int numOfQueues = 0;
+  /**
+   * A list collection of customers.
+   */
 
+  private final List<Customer> list;
+  /**
+   * An ID for this queue.
+   */
+
+  private final int id;
+  /**
+   * The number of queues created so far.
+   */
+
+  private static int numOfQueues = 0;
+  
+   /**
+   * Create an empty customer queue.
+   */
   public Queue(){
     this.list = new LinkedList<Customer>();
     this.id = Queue.numOfQueues;
     Queue.numOfQueues++;
   }
 
-  public boolean add(Customer customer){
-    return this.list.add(customer);
+  /**
+   * Add a customer to the end of this queue.
+   * 
+   * @param c The customer who joins this queue.
+   * @return true if the customer joins the queue, false otherwise.
+   */
+  public boolean add(Customer c){
+    return this.list.add(c);
   }
-
+  
+  /**
+   * Add a customer to the front of this queue.
+   * 
+   * @param c The customer who joins this queue.
+   * @return true if the customer joins the queue, false otherwise.
+   */
   //Pushes customer to top of the stack.
-  public void bump(Customer customer){
-    this.list.add(0, customer);
+  public void bump(Customer c){
+    this.list.add(0, c);
   }
 
-  public void remove(Customer customer){
-    this.list.remove(customer);
+  /**
+   * Remove a customer from this queue.
+   * 
+   * @param c The customer who is in this queue
+   * @return true if the customer is removed, false otherwise.
+   */
+  public void remove(Customer c){
+    this.list.remove(c);
   }
 
+  /**
+   * Get size of this queue.
+   * 
+   * @return size of current queue.
+   */
   public int size(){
     return this.list.size();
   }
 
-  public boolean contains(Customer customer){
-    return this.list.contains(customer);
+  /**
+   * Checks if a customer exist in this queue.
+   * 
+   * @param c The customer who is in this queue.
+   * @return true if the customer exists, false otherwise.
+   */
+  public boolean contains(Customer c){
+    return this.list.contains(c);
   }
 
-  public int indexOf(Customer customer){
-    return this.list.indexOf(customer);
+  /**
+   * Gets customer's position in this queue.
+   * 
+   * @param c The customer who is in this queue.
+   * @return index of customer's position in this queue.
+   */
+  public int indexOf(Customer c){
+    return this.list.indexOf(c);
   }
 
+  /**
+   * Remove the next customer from this queue.
+   * @return The customer at the head of the queue, or null if
+   *     the queue is empty.
+   */
   public Customer next(){
     if(this.list.isEmpty()){
       return null;
@@ -48,6 +104,10 @@ class Queue{
     }
   }
 
+  /**
+   * Return a string representation of this queue.
+   * @return The id of this queue with prefix "Q" in string form.
+   */
   public String toString(){
     return "Q" + id;
   }
